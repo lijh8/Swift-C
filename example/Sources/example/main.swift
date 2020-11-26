fcc9fc64822d4802e66f5f02272812fa29a459dd
ljh <ljh@Mac-mini.local>
2020-11-26 18:34:15 +0800

    Update

--- example/Sources/example/main.swift
+++ example/Sources/example/main.swift
@@ -1,6 +1,6 @@
 import clib
 
-// you can also use Glibc for fputs(3)
+// you can also import Glibc for fputs(3)
 // then no need to mention: header "/.../stdio.h", link "c", in *.modulemap
 //import Glibc
 
