08043365ad7964eef5574a82318dfe31cea5c042
ljh <ljh@Mac-mini.local>
2020-11-26 18:30:30 +0800

    Update

--- example/Sources/example/main.swift
+++ example/Sources/example/main.swift
@@ -1,4 +1,8 @@
 import clib
 
+// you can also use Glibc for fputs(3)
+// then no need to mention: header "/.../stdio.h", link "c", in *.modulemap
+//import Glibc
+
 hello()
 fputs("Swift calls fputs(3)\n", stdout)
