5e5428cafef676f260d19cd71165d458107aa36f
ljhm <9011764+ljhm@users.noreply.github.com>
2023-05-01 11:59:44 +0800

    Add files via upload

--- helloSwift/hello/build.sh
+++ helloSwift/hello/build.sh
@@ -1,15 +1,25 @@
 # bash build.sh
 # bash build.sh clean
 
+# install directory layout
+
+# |-- app
+# |   `-- hello
+# `-- lib
+#     |-- libbar.dylib
+#     |-- libbar.dylib.1
+#     `-- libbar.dylib.1.2.3
+
 if [[ $1 == "clean" ]]; then
-    gmake -C `pwd`/Sources clean
+    gmake -C "`pwd`/Sources" clean
     swift package clean
 else
     # release: NDEBUG=1
-    gmake -C `pwd`/Sources # NDEBUG=1
+    gmake -C "`pwd`/Sources" # NDEBUG=1
     # debug/release
     swift build -c debug \
-        -Xswiftc -LSources/build/bar \
+        -Xlinker -LSources/build/bar \
         -Xlinker -lbar \
-        -Xlinker -rpath -Xlinker Sources/build/bar
+        -Xlinker -rpath -Xlinker "`pwd`/Sources/build/bar" \
+        -Xlinker -rpath -Xlinker @executable_path/../lib
 fi
